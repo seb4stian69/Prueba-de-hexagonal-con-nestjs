1130c50f5096b85af4f76e143cd6f671d2b2f0a8
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-04-09 15:42:34 -0500

    CommandHandler de crear cliente listo

--- src/Infraestructure/AppService/CommandHandler/CommandHandler.ts
+++ src/Infraestructure/AppService/CommandHandler/CommandHandler.ts
@@ -12,6 +12,8 @@ import { EditProductUseCase } from 'src/Application/UseCase/EditProductUseCase';
 import { DeleteProductUseCase } from 'src/Application/UseCase/DeleteProductUseCase';
 import { ProductRepository } from '../Repository/ProductRepository';
 import { Shop } from 'src/Domain/Shop';
+import { CreateClientCommand } from 'src/Domain/Commands/CreateClientCommand';
+import { CreateClientUseCase } from 'src/Application/UseCase/CreateClientUseCase';
 
 const inCommand: string = '[En el comando]'
 
@@ -136,6 +138,29 @@ export class BuyProductHandler implements ICommandHandler<BuyProductCommand> {
     let usecase = new BuyProductUseCase(this.repository);
     const shop = await usecase.apply(command);
 
+    this.publisher.mergeObjectContext(shop);
+    shop.commit();
+
+  }
+
+} 
+
+/* + ------------------------------ | Create Client | ------------------------------ + */
+
+@CommandHandler(CreateClientCommand)
+export class CreateClientHandler implements ICommandHandler<CreateClientCommand> {
+
+  constructor(
+    private readonly publisher: EventPublisher,
+  ) {}
+
+  async execute(command: CreateClientCommand) {
+
+    console.log('\n+----------------------------------------------------+');
+    console.log(`\x1b[32m${inCommand}\x1b[0m` + ' Creando cliente ...');
+
+    let usecase = new CreateClientUseCase();
+    const shop = await usecase.apply(command);
     this.publisher.mergeObjectContext(shop);
     shop.commit();
 
