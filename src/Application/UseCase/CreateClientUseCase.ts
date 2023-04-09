b91895dc7265253f215dd896028bc306ce208272
Sebastian Santis <sebastiandasanhe_1287@hotmail.com>
2023-04-09 15:39:01 -0500

    Caso de uso de crear cliente completado

---
+++ src/Application/UseCase/CreateClientUseCase.ts
@@ -0,0 +1,16 @@
+import { Shop } from "../../Domain/Shop";
+import { CreateClientCommand } from "src/Domain/Commands/CreateClientCommand";
+import { getClientCreatedData } from "../Utils/ConvertToEntity";
+
+export class CreateShopUseCase{
+
+  constructor(){/* Void */}
+
+  async apply(command: CreateClientCommand):Promise<Shop> {
+    const shop:Shop = new Shop();
+    shop.createClient(getClientCreatedData(command))
+    return shop;
+  }
+      
+}
+
